95424691bea1df802bfbcb051d0f2e5f8e93608c
DanielCarrami <danielcarrami@gmail.com>
2020-08-26 12:52:07 -0500

    style corrections

--- w3/leaflet-example/src/app/map/map.component.ts
+++ w3/leaflet-example/src/app/map/map.component.ts
@@ -1,5 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { icon, Marker } from 'leaflet';
+import { MarkerService } from '../_services/marker.service';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -8,10 +26,12 @@ import * as L from 'leaflet';
 export class MapComponent implements OnInit {
   private map;
 
-  constructor() { }
+  constructor(private markerService: MarkerService) { }
 
   ngOnInit(): void {
       this.initMap();
+      
+    this.markerService.makeCapitalMarkers(this.map);
   }
 
   private initMap(): void {
